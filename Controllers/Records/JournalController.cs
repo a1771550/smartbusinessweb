fb9e23ab572479ae7407bdccfb2c4999156bda24
Kevin LAU <kevinlau@united.com.hk>
2023-11-16 18:00:40 +0800

    starting Journal

---
+++ Controllers/Records/JournalController.cs
@@ -0,0 +1,20 @@
+﻿using SmartBusinessWeb.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartBusinessWeb.Controllers.Records
+{
+	[CustomAuthenticationFilter]
+	public class JournalController : BaseController
+    {
+		[HandleError]
+		[CustomAuthorize("reports", "boss", "admin", "superadmin")]
+		public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
