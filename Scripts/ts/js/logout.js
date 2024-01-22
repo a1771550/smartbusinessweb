584996483d43588379bd6ab8d6217e37ef8d14cb
Kevin LAU <kevinlau@united.com.hk>
2024-01-22 11:17:51 +0800

    update packages

---
+++ Scripts/ts/js/logout.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:df9978304d1f3cdc424aeecb542c0f5aaa3250c0cebb3d6157c74c3f882cbe05
+size 865
---
+++ Scripts/ts/logout.ts
@@ -0,0 +1,29 @@
+﻿let unauthorized = $("#loginblk").length ? $("#loginblk").data("unauthorized") === 'True' : false;
+let enablecheckdayends = $("#loginblk").length ? $("#loginblk").data("enablecheckdayends") === 'True' : false;
+$(document).on('click', '#btnLogout', function () {
+    if (unauthorized) {
+        logout();
+    }
+    else {
+        if (enablecheckdayends) {
+            getRemoteData('/Api/CheckIfDayendsDone', {}, checkIfDayendsDoneOk, getRemoteDataFail);
+        }
+        else {
+            logout();
+        }
+    }
+});
+
+function logout() {
+    $("#logoutForm").trigger("submit");
+}
+
+function checkIfDayendsDoneOk(data) {
+    //console.log('checkdayenddata:', data);
+    if (data.msg === 'done') {
+        $("#logoutForm").trigger("submit");
+    }
+    else {
+        window.location.href = '/POSFunc/Dayends';
+    }
+}
